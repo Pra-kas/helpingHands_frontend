cf453e9b1b55ce05c9843c0adad1e1285e212d31
Gopinath.s <gopinath.s2022cce@sece.ac.in>
2024-04-20 15:04:17 +0530

    initial commit

---
+++ android/app/src/main/kotlin/com/example/helpinghandsfrontend/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.helpinghandsfrontend
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/component/view/volunteer_signup_view.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class VolunteerSignupView extends StatefulWidget {
+  const VolunteerSignupView({super.key});
+
+  @override
+  State<VolunteerSignupView> createState() => _VolunteerSignupViewState();
+}
+
+class _VolunteerSignupViewState extends State<VolunteerSignupView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
